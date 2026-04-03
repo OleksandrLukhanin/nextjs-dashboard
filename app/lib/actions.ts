d93b44c8a3daafbbfcfa22d33e7d3b69ac0f4237
Oleksandr Lukhanin <oleksandr.lukhanin@gmail.com>
2026-04-03 14:50:08 +0300

    type mismatch fix

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -69,8 +69,7 @@ export async function deleteInvoice(id: string) {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
   } catch (error) {
-    console.error(error);
-    return { message: "Database Error: Failed to Delete Invoice." };
+    console.error("Database Error: Failed to Delete Invoice", error);
   }
   revalidatePath("/dashboard/invoices");
 }
